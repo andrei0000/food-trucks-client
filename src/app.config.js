8354945e089bfedef124ae070a15fcbb488c13c3
andrei0000 <andrei.nilin@gmail.com>
2017-03-22 17:11:00 +0300

    server url

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
   angular
     .module('app.config', [])
     .constant('AppConfig', {
-      SERVER_URL: 'http://127.0.0.1:9000',
+      SERVER_URL: 'https://food-trucks-api-demo.herokuapp.com',
       DEFAULT_LATITUDE: 37.75962101544518,
       DEFAULT_LONGITUDE: -122.42932319641113,
       MAP_ZOOM: 12
